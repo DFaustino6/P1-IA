4542ecfcb3b58c5e379429149d7c882b58870dbb
DFaustino6 <a61171@ualg.pt>
2020-10-22 22:26:03 +0100

    Update Stacks.java

--- src/app/Stacks.java
+++ src/app/Stacks.java
@@ -12,18 +12,15 @@ public class Stacks implements Ilayout, Cloneable {
     public Stacks(String str) {
         if(str.length() <dim) throw new IllegalStateException("Invalid arg in Board constructor");
 
-        List<Stack<Character>> stacks = new ArrayList<Stack<Character>>();
-        Stack<Character> stack = new Stack<Character>();
-        for (int i = 0; i < str.length(); i++) {
-            stack.push(str.charAt(i));
-            if(str.charAt(i)==' '){
-                i++;
-                stacks.add(stack);
-                stack = new Stack<Character>();
-            }
-        }
-        System.out.println(stacks.size());
-
+        this.stacks = new ArrayList<Stack<Character>>(dim);
+        
+        for (int i = 0; i < dim; i++) 
+            this.stacks.add(new Stack<Character>());
+        
+        int nStack=0;
+        for (int i = 0; i < str.length(); i++)
+            if(str.charAt(i)!=' ') stacks.get(nStack).push(str.charAt(i));
+            else nStack++;
     }
 
     @Override
@@ -49,10 +46,9 @@ public class Stacks implements Ilayout, Cloneable {
         if(stackFormat.equals("")){
             for (Stack<Character> stack : stacks) {
                 String s="";
-                for (Character character : stack) {
+                for (Character character : stack)
                     s += character;
-                }
-                stackFormat += "["+ s +"]";
+                stackFormat += "["+ s +"]\r\n";
             }
         }
         return stackFormat;
